ec50a3795d77269e37be331008ed256e1841a80e
Ron Silva <rdsilva.dev@gmail.com>
2023-06-03 01:11:40 +0100

    up

--- exercícios/ex008.js
+++ exercícios/ex008.js
@@ -1,6 +1,7 @@
-var vel = 60.5
+var vel = 12
 console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { // condição simples! não tem senão
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
 
-
-console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 console.log(`Dirija sempre usando cinto de segurança!`)
