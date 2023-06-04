44dbe0846f59f26804a9633a70d39defde40ecec
Ron Silva <rdsilva.dev@gmail.com>
2023-06-04 02:55:26 +0100

    up

---
+++ exercícios/ex008/ex013.js
@@ -0,0 +1,61 @@
+// Condição múltipla
+
+
+// Forma:
+
+// switch (expressão) {
+//     case valor 1: 
+//     ação
+//     break
+
+//     case valor 2: 
+//     ação
+//     break
+
+//     case valor 3: 
+//     ação
+//     break
+
+//     default:
+// }
+
+var agora = new Date()
+var diaSem = agora.getDay()
+/*
+    0 = Domingo
+    1 =  Segunda
+    2 = Terça
+    3 = Quarta
+    4 = Quinta
+    5 = Sexta
+    6 = Sábado
+*/
+
+// console.log(diaSem)
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('[ERRO] Dia inválido!')
+        break
+}
---
+++ exercícios/ex009/ex014.js
@@ -0,0 +1,26 @@
+/* Estruturas de repetição: while, do while e for
+
+while: repetições com teste no início
+do while: repetições com teste no final
+for: repetições com controle
+
+
+estrutura do while:
+
+while (condição) {
+    ação até o objeto - quando for concluído, acabe o loop
+} 
+
+*/
+
+var c = 1
+while (c < 8) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
